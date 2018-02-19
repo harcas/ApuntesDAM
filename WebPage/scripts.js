43293d949d92d5e86d06774f2136ad77451773ae
KevinDavidCabreraRodriguez <34677249+KevinDavidCabreraRodriguez@users.noreply.github.com>
2018-02-19 10:19:39 +0000

    Add files via upload
    - Changed nav menu, now it only uses 20% of the screen, instead of 100%
    - Redid the nav menu, only displays the names of classes
    - Header now more minimalistic
    - Disabled the footer temporarily as it doesn't contains usefull items yet

--- WebPage/scripts.js
+++ WebPage/scripts.js
@@ -1,17 +1,8 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.width = "20%";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-}
-
-function openPract() {
-  if (document.getElementById("dropdown-content").style.display == "flex") {
-    document.getElementById("dropdown-content").style.display = "none";
-  }
-  else {
-    document.getElementById("dropdown-content").style.display = "flex";
-  }
 }
